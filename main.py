a873807bee9661af374ebac12b2c3abe2c422c01
O1SA <olivier.st.amand.1@gmail.com>
2019-04-03 11:00:16 -0400

    Add below thresh mode

--- main.py
+++ main.py
@@ -18,14 +18,14 @@ client=io.Client(secrets['ssid'],
                 )
 client.connect()
 # plant monitor board 
-board = Board(thresh=thresh_moisture, np_color=neopixel_color)
+board = Board(thresh=thresh_moisture, np_color=neopixel_color, thresh_n=thresh_n)
 
 """Main 
 """
 last_upd_time = 0
 while True:
     # update current mode 
-    if board.is_normal_mode() and board.is_calibration_on():
+    if not board.is_calibration_mode() and board.is_calibration_on():
         print('set to calibration mode') 
         board.set_to_calibration_mode()
     elif board.is_calibration_mode() and not board.is_calibration_on(): 
@@ -33,7 +33,7 @@ while True:
         board.set_to_normal_mode()
 
     # normal mode
-    if board.is_calibration_mode():
+    if not board.is_calibration_mode():
         now = time.ticks_ms()
         delta = time.ticks_diff(now, last_upd_time)
         print('delta_t: {}'.format(delta))
@@ -42,12 +42,12 @@ while True:
             is_below, value = board.is_moisture_below_thresh()
             print('moisture {}'.format(value))
             client.send_data('moisture-sensor', value)
-            if is_below:
-                print('animate')
-                board.np_animate_start()
-            else: 
-                print('set np off')
-                board.np_off()
+            if board.is_normal_mode() and is_below:
+                print('set to below thresh mode')
+                board.set_to_below_thresh_mode()
+            elif board.is_below_mode() and not is_below: 
+                print('set to normal mode')
+                board.set_to_normal_mode()
         board.np_animate_update()
 
     # calibration mode. nothing to do on update 
--- plant_monitor/board.py
+++ plant_monitor/board.py
@@ -10,15 +10,19 @@ class Board:
     CAL = 1
     BELOW_THRESH = 2
 
-    def __init__(self, thresh=300, np_color=(33,23,125)):
+    def __init__(self, thresh=300, thresh_n=5, np_color=(33,23,125)):
         self.thresh = thresh
-        self.np_color = np_color 
+        self.np_color = np_color
+        self.thresh_n = thresh_n
+
+        # current count below thresh
+        self.count_thresh = None 
 
         # moisture sensor pwr connected to GPI14.
         self.pwr_sensor = machine.Pin(14, machine.Pin.OUT)
 
-        # will be input or ADC depending on mode
-        self.moisture_sensor = None
+        # moisture sensor analog signal on ADC 
+        self.moisture_sensor = self.moisture_sensor = ADC(0)
 
         # calibration mode pin GPIO2 
         self.calibration_pin = machine.Pin(2, machine.Pin.IN)
@@ -40,7 +44,12 @@ class Board:
 
     def is_moisture_below_thresh(self):
         value = self.get_moisture()
-        return (value < self.thresh, value)
+        if value < self.thresh:
+            self.count_thresh += 1 
+        else:
+            if self.count_thresh > 0:
+                self.count_thresh -= 1  
+        return (self.count_thresh > self.thresh_n, value)
     
     def is_normal_mode(self):
         return self.current_mode == Board.NORMAL
@@ -48,13 +57,14 @@ class Board:
     def is_calibration_mode(self):
         return self.current_mode == Board.CAL
 
+    def is_below_mode(self):
+        return self.current_mode == Board.BELOW_THRESH
+
     # led on = calibration mode 
     def is_calibration_on(self):
         return not self.calibration_pin.value()
 
     def set_to_calibration_mode(self):
-        # set ADC to input to make sure ADC will not go over 1V during calibration
-        self.moisture_sensor = machine.Pin(17, machine.Pin.IN)
         # sensor always on 
         self.pwr_sensor.on()
         # np always off 
@@ -62,19 +72,26 @@ class Board:
         self.current_mode = Board.CAL
 
     def set_to_normal_mode(self):
-        # moisture sensor analog signal on ADC 
-        self.moisture_sensor = ADC(0)
+        self.count_thresh = 0
         self.pwr_sensor.off()
         self.np_off()
         self.current_mode = Board.NORMAL
+
+    def set_to_below_thresh_mode(self):
+        # temp to be updated 
+        for i in range(self.np_n):
+            self.np[i] = self.np_color
+        self.np.write()
+        self._is_np_on = True 
+        self.current_mode = Board.BELOW_THRESH
 
     def is_np_off(self):
         return not self._is_np_on
 
     def np_off(self):
         for i in range(self.np_n):
-            self.np[i] = 0 
-        np.write()
+            self.np[i] = (0,0,0) 
+        self.np.write()
         self._is_np_on = False 
 
     # will animate neopixels until off is called 
@@ -83,8 +100,7 @@ class Board:
     # 110 mA at 25% 
     # 370 mA at 100%
     def np_animate_update(self):
-        self._is_np_on = True  
-
-    def np_animate_start(self):
         pass
+
+
 
