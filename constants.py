477c63754dae16e26e3b3efb108256ff46bf70a1
O1SA <olivier.st.amand.1@gmail.com>
2019-04-03 20:42:50 -0400

    Add board layout

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 delay_upd = 60 # in seconds
-thresh_moisture = 600 # below will trigger neopixels 
+thresh_moisture = 300 # below will trigger neopixels 
 thresh_n = 2 # number of measurement below before triggering 
 neopixel_color = (33,23,125)
